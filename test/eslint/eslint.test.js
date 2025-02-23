e77cc2df49df514fa26007471f84ba53e374455c
Kriss Strikis <kstrikis@gmail.com>
2025-02-23 15:14:01 -0600

    fix test of lint rule

--- test/eslint/eslint.test.js
+++ test/eslint/eslint.test.js
@@ -2,7 +2,19 @@ const { ESLint } = require('eslint');
 const { restrictedSyntax } = require('@/../scripts/webpack-util');
 
 test('eslint no-restricted-syntax', async () => {
-  const linter = new ESLint();
+  const linter = new ESLint({
+    useEslintrc: false,
+    baseConfig: {
+      parserOptions: {
+        ecmaVersion: 2021
+      },
+      rules: {
+        'no-restricted-syntax': ['error', ...restrictedSyntax]
+      }
+    }
+  });
+  
+  // Test that the rule catches all restricted patterns
   const code = restrictedSyntax.map(r => r.code + ';').join('');
   const expected = restrictedSyntax.map(r => (delete r.code, r.message));
   for (const path of ['', '/content', '/web']) {
@@ -14,4 +26,18 @@ test('eslint no-restricted-syntax', async () => {
     expect(unexpected).toEqual([]);
     expect(missed).toEqual([]);
   }
+
+  // Test that safe code passes
+  const safeCode = 'const x = 1; function foo() { return x + 1; }';
+  const safeResult = await linter.lintText(safeCode);
+  expect(safeResult[0].messages).toEqual([]);
+
+  // Test that a specific restricted pattern is caught
+  const unsafeCode = 'Object.assign({}, {foo: 1});';
+  const unsafeResult = await linter.lintText(unsafeCode);
+  // console.log('Messages for unsafe code:', unsafeResult[0].messages);
+  expect(unsafeResult[0].messages.map(m => m.message)).toEqual([
+    'Using potentially spoofed methods in an unsafe environment',
+    'Using potentially spoofed methods in an unsafe environment'
+  ]);
 });
